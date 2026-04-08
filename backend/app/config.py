6dbae395a41035936083179c4045a7fc8d7570b8
sworcery <sworcery@gmail.com>
2026-04-08 01:18:24 -0600

    Bump version to 1.6.0

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 class Settings(BaseSettings):
     APP_NAME: str = "ChannelHoarder"
-    APP_VERSION: str = "1.5.7"
+    APP_VERSION: str = "1.6.0"
     DEBUG: bool = False
     LOG_LEVEL: str = "info"
 
